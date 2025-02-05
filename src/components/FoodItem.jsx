48c2ff4c9b0319169742a327bd4255d21bd54566
SRISHTI <Srishtiludhiana@gmail.com>
2025-02-05 20:51:36 +0530

    Version2

--- src/components/FoodItem.jsx
+++ src/components/FoodItem.jsx
@@ -1,30 +1,25 @@
 import { IMG_CDN_URL } from "../constant";
 
-const FoodItem = ({
-    name = "Unknown Food",
-    description = "No description available",
-    cloudinaryImageId = "default_image_id",
-    price = 0
-}) => {
-    console.log("FoodItem Props:", {
-        name,
-        description,
-        cloudinaryImageId,
-        price,
-    });
+const FoodItem = ({ name, description, cloudinaryImageId, price }) => {
+  console.log("FoodItem Props:", {
+    name,
+    description,
+    cloudinaryImageId,
+    price,
+  });
 
-    return (
-        <div className="w-[250px] h-[420px] p-4 my-5 mx-4 shadow-lg bg-blue-100 hover:bg-amber-50 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
-            <img
-                className="rounded-md h-32 w-full object-cover"
-                src={IMG_CDN_URL + cloudinaryImageId}
-                alt={name}
-            />
-            <h2 className="font-bold text-xl mt-2">{name}</h2>
-            <h3 className="text-gray-600">Rupees: {price / 100}</h3>
-            <h5 className="text-gray-600">{description}</h5>
-        </div>
-    );
+  return (
+    <div className="w-[250px] h-[420px] p-4 my-5 mx-4 shadow-lg bg-blue-100 hover:bg-amber-50 rounded-md transition duration-300 ease-in-out transform hover:scale-105">
+      <img
+        className="rounded-md h-32 w-full object-cover"
+        src={IMG_CDN_URL + cloudinaryImageId}
+        alt={name}
+      />
+      <h2 className="font-bold text-xl mt-2">{name}</h2>
+      <h3 className="text-gray-600">Rupees: {price / 100}</h3>
+      <h5 className="text-gray-600">{description}</h5>
+    </div>
+  );
 };
 
 export default FoodItem;
